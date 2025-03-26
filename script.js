902e65edcd93c4a2659b5c441c53c8258407c027
Jeff Seah <seahks8@gmail.com>
2025-03-26 20:23:05 +0800

    Update script.js
    car dropdown

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-    const form = document.getElementById('contact-form');
-    const responseMessage = document.getElementById('response-message');
+const form = document.getElementById('contact-form');
+const responseMessage = document.getElementById('response-message');
 
-    form.addEventListener('submit', async (event) => {
-      event.preventDefault();
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-      const formData = {
-        name: document.getElementById('name').value,
-        contact: document.getElementById('contact').value,  
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-      };
+  const formData = {
+    name: document.getElementById('name').value,
+    hp: document.getElementById('hp').value,
+    email: document.getElementById('email').value,
+    car: document.getElementById('car').value,
+    message: document.getElementById('message').value
+  };
 
-      try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbyOTdOW-_v7Hws2sTYyfqVl7j9sgL8NZsi2mNvI5W-iIk3JRO39CgbxXWV3Oy-kZHjS/exec', {
-          method: 'POST',
-          body: JSON.stringify(formData)
-        });
-
-        if (response.ok) {
-          responseMessage.textContent = 'Thank you! Your message has been sent.';
-          form.reset();
-        } else {
-          responseMessage.textContent = 'Error submitting the form. Please try again.';
-        }
-      } catch (error) {
-        responseMessage.textContent = 'An error occurred. Please try again.';
-      }
+  try {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbzbErEC_L2_-m-rawWBTf7aEYUeck3VuzeqjY6MUbZJ963tt9lAxvZgfU-g5FyGVPjq/exec', {
+      method: 'POST',
+      body: JSON.stringify(formData)
     });
+
+    if (response.ok) {
+      responseMessage.textContent = 'Thank you! Your message has been sent.';
+      form.reset();
+    } else {
+      responseMessage.textContent = 'Error submitting the form. Please try again.';
+    }
+  } catch (error) {
+    responseMessage.textContent = 'An error occurred. Please try again.';
+  }
+});
